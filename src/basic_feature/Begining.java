14c1a236f6fcfaf12699c79dcbf7f73efe5325c4
Kalipada Kamilya <Kalipada.Kamilya@gmail.com>
2021-03-18 18:23:23 +0530

    Program ready to be run in Jenkin

--- src/basic_feature/Begining.java
+++ src/basic_feature/Begining.java
@@ -4,7 +4,7 @@ public class Begining {
 
 	public static void main(String[] args) {
 		System.out.println("hello, world");
-
+		System.out.println("Program ready to be executed in Jenkins");
 	}
 
 }
